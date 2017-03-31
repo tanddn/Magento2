0d2ac1d293589bf70935f85ed0b281904da76239
Lộc Nguyễn <npvinhloc@gmail.com>
2017-03-31 16:12:37 +0700

    apply themes

---
+++ app/etc/env.php
@@ -0,0 +1,60 @@
+<?php
+return array (
+  'cache_types' => 
+  array (
+    'compiled_config' => 1,
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'customer_notification' => 1,
+    'full_page' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+  ),
+  'backend' => 
+  array (
+    'frontName' => 'admin_smacnew',
+  ),
+  'crypt' => 
+  array (
+    'key' => 'a334920cfa0a7620dccfd82f7252b4ed',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => '103.74.119.72',
+        'dbname' => 'magento2',
+        'username' => 'loc.nguyen',
+        'password' => 'abcde12345-',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'developer',
+  'install' => 
+  array (
+    'date' => 'Thu, 30 Mar 2017 16:53:16 +0000',
+  ),
+);
