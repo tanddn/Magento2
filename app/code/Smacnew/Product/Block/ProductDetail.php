071feca04ca1fb2adec2312a5beb5425dbd59586
Lộc Nguyễn <npvinhloc@gmail.com>
2017-05-01 10:21:30 +0700

    done product_detail layout

---
+++ app/code/Smacnew/Product/Block/ProductDetail.php
@@ -0,0 +1,14 @@
+<?php
+namespace Smacnew\Product\Block;
+
+class ProductDetail extends \Magento\Framework\View\Element\Template
+{
+    public function getHelloWorldTxt()
+    {
+        return 'Hello world!';
+    }
+
+    public function getProductDetail($productID){
+        return 'Hello world!';
+    }
+}
---
+++ app/code/Smacnew/Product/Block/RelatedProducts.php
@@ -0,0 +1,15 @@
+<?php
+namespace Smacnew\Product\Block;
+
+class RelatedProducts extends \Magento\Framework\View\Element\Template
+{
+    public function getHelloWorldTxt()
+    {
+        return 'Hello world!';
+    }
+
+    public function getRelatedProducts($productID){
+        //TODO: this function will get all products that related to $productID
+        return 'Hello world!';
+    }
+}
